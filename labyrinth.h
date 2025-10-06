11027ffc60883c8e2de285b633b18f86c5ee5662
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-10-06 19:00:10 +0200

    feat: Add all cell contents, possible points to cell and vectors

--- labyrinth.h
+++ labyrinth.h
@@ -6,14 +6,22 @@
 #define PROJECT_LABYRINTH_H
 
 #define LENGTH (int) 11
-#define WIDTH (int) 25
+#define WIDTH (int) 13
 
-enum cell { WALL = '#', VOID = ' ', EXIT = '_', PLAYER = 'o' };
+typedef enum cell_content { WALL = '#', VOID = ' ', EXIT = '-', PLAYER = 'o', KEY = '*', CHEST= '+', TRAP = '~' } cell_content;
+typedef enum direction { LEFT, RIGHT, UP, DOWN } direction;
 
-enum direction { LEFT, RIGHT, UP, DOWN };
+typedef struct cell {
+    cell_content cell_content;
+    int id;
+    int point;
+} cell;
 
-char labyrinth[LENGTH+1][WIDTH+1];
 
-time_t startingTime;
+int vectors[5][3] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+cell labyrinth[LENGTH+1][WIDTH+1];
+
+time_t starting_time;
 
 #endif //PROJECT_LABYRINTH_H
